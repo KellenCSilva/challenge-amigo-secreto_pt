1bba1845cda73025466208c631278b6e40ebeaba
Kellen Silva <kaahsrsouza@gmail.com>
2025-08-11 20:19:30 -0300

    Adicionar exibir e sortear amigo

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigo() {
+    let amigoInput = document.getElementById("amigo");
+    let amigo = amigoInput.value.trim();
+    if(amigo !== "") {
+        amigos.push(amigo);
+        amigoInput.value = "";
+        exibirAmigos();
+    } else {
+        alert("Por favor, insira um nome válido.");
+        return;
+    }
+}
+
+function exibirAmigos() {
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+    amigos.forEach(function(amigo) {
+        let item = document.createElement("li");
+        item.textContent = amigo;
+        lista.appendChild(item);
+    });
+}
+
+function sortearAmigo() {
+    amigos.sort();
+    exibirAmigos();
+    let sorteado = amigos[Math.floor(Math.random() * amigos.length)];
+    alert("Amigo sorteado: " + sorteado);
+}
+
+
